62b1bfc2ff50d43b52d1acafcf3e456ab7c7f6d3
nico151999 <nico.feulner@gmail.com>
2017-06-26 19:32:11 +0200

    Cleanup and update some files

--- app/src/main/java/de/nilix/simplebackup/MainActivity.java
+++ app/src/main/java/de/nilix/simplebackup/MainActivity.java
@@ -1,15 +1,12 @@
 package de.nilix.simplebackup;
 
 import android.support.design.widget.TabLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,15 +28,6 @@ public class MainActivity extends AppCompatActivity {
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mViewPager);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
     }
 
