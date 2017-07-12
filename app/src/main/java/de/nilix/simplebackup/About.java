3b3ffaec089d1d148a7babc5573e4cf5da2e7773
nico151999 <nico.feulner@gmail.com>
2017-07-12 16:01:25 +0200

    Improvements, overflow menu, settings activity & about activity
    in short:many additions which will be improved in upcoming commits.

---
+++ app/src/main/java/de/nilix/simplebackup/About.java
@@ -0,0 +1,10 @@
+package de.nilix.simplebackup;
+
+import android.app.Activity;
+
+/**
+ * Created by nico99 on 12.07.17.
+ */
+
+public class About extends Activity {
+}
--- app/src/main/java/de/nilix/simplebackup/MainActivity.java
+++ app/src/main/java/de/nilix/simplebackup/MainActivity.java
@@ -1,12 +1,13 @@
 package de.nilix.simplebackup;
 
+import android.content.Intent;
 import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -42,6 +43,9 @@ public class MainActivity extends AppCompatActivity {
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mViewPager);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
     }
 
     @Override
@@ -53,17 +57,20 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch(item.getItemId()) {
+            case R.id.action_settings:
+                Intent intent1 = new Intent("de.nilix.simplebackup.settingsitem");
+                this.startActivity(intent1);
+                break;
+            case R.id.action_about:
+                Intent intent2 = new Intent("de.nilix.simplebackup.aboutitem");
+                this.startActivity(intent2);
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
         }
 
-        return super.onOptionsItemSelected(item);
+        return true;
     }
 
     private void setupViewPager(ViewPager viewPager) {
