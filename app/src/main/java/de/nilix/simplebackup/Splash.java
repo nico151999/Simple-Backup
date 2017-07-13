0740744a1f96479586762188356d29616f6be3a9
nico151999 <nico.feulner@gmail.com>
2017-07-13 12:19:45 +0200

    Add splash screen and app counter [2/2]

---
+++ app/src/main/java/de/nilix/simplebackup/Splash.java
@@ -0,0 +1,21 @@
+package de.nilix.simplebackup;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by nico99 on 13.07.17.
+ */
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
